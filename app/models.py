b9f407a7a86e3e75eabe8a01f22d40144bcdf9bf
brucew2099 <alankwok1@gmail.com>
2022-10-31 16:45:19 -0400

    Modified models

--- app/models.py
+++ app/models.py
@@ -50,6 +50,7 @@ class Patient(Base):
     """
     __tablename__ = 'Patients'
     id = Column('Id', Integer, primary_key=True)
+    prefix = Column('Prefix', String(10), nullable=True)
     first_name = Column('FirstName', String(100), nullable=False)
     last_name = Column('LastName', String(100), nullable=False)
     date_of_birth = Column('DateOfBirth', Date, nullable=False)
@@ -64,41 +65,32 @@ class Patient(Base):
     need_interpretor = Column('NeedInterpretor', Boolean, nullable=False, default=False)
     referral_first_name = Column('ReferralFirstName', String(255), nullable=True)
     referral_last_name = Column('ReferralLastName', String(255), nullable=True)
-    prefix = Column('Prefix', String(10), nullable=True)
-    permanent_address_id = Column('PermanentAddressId', ForeignKey('Addresses.Id'), nullable=False)
-    temp_address_id = Column('TempAddressId', ForeignKey('Addresses.Id'), nullable=True)
-    phone_number_1_id = Column('PhoneNumber1Id', ForeignKey('Phones.Id'), nullable=True)
-    phone_number_2_id = Column('PhoneNumber2Id', ForeignKey('Phones.Id'), nullable=True)
-    preferred_contact_id = Column('PreferredContactId',
+    preferred_contact_id = Column('PreferredContactId',\
         ForeignKey('PreferredContacts.Id'), nullable=False)
     language_id = Column('LanguageId', ForeignKey('Languages.Id'), nullable=True)
     coverage_type_id = Column('CoverageTypeId', ForeignKey('CoverageTypes.id'), nullable=True)
     referral_type_id = Column('ReferralTypeId', ForeignKey('ReferralTypes.Id'), nullable=True)
-    insurance_card_front_id = Column('InsuranceCardFrontId', ForeignKey('Images.Id'),
-        nullable=False)
-    insurance_card_back_id = Column('InsuranceCardBackId', ForeignKey('Images.Id'),
-        nullable=True)
+    permanent_address_id = Column('PermanentAddressId', ForeignKey('Address.Id'), nullable=True)
+    temporary_address_id = Column('TemporaryAddressId', ForeignKey('Address.Id'), nullable=True)
+    phone_1_id = Column('Phone1Id', ForeignKey('Phone.Id'), nullable=True)
+    phone_2_id = Column('Phone2Id', ForeignKey('Phone.Id'), nullable=True)
+    insurance_card_front_id = Column('InsuranceCardFrontId', ForeignKey('Image.Id'), nullable=True)
+    insurance_card_back_id = Column('InsuranceCardBackId', ForeignKey('Image.Id'), nullable=True)
 
-    permanent_address = relationship('Addresses')
-    temp_address = relationship('Addresses')
-    phone_number_1 = relationship('Phones')
-    phone_number_2 = relationship('Phones')
-    preferred_contact = relationship('PreferredContacts')
-    language = relationship('Languages')
-    coverage_type = relationship('CoverageTypes')
-    referral_type = relationship('ReferralTypes')
-    insurance_card_front = relationship('Images')
-    insurance_card_back = relationship('Images')
+    intake = relationship('Intake', backref='patient')
 
-    def __init__(self, patient_id=0, first_name=None, last_name=None, date_of_birth=None,
+    def __init__(self, patient_id=0, prefix=None, first_name=None, last_name=None, date_of_birth=None,
             gender=None, age=None, parent_first_name=None, parent_last_name=None, email=None,
             is_us_citizen=True, need_visa=None,
-            dates_available=None, need_interpretor=None, insurance_card_front_id=0,
-            insurance_card_back_id=0, referral_first_name=None, referral_last_name=None,
-            permanent_address_id=0, temp_address_id=0, phone_number_1_id=0,
-            phone_number_2_id=0, preferred_contact_id=0, language_id=0,
-            coverage_type_id=0, referral_type_id=0):
+            dates_available=None, need_interpretor=None,
+            referral_first_name=None, referral_last_name=None,
+            preferred_contact_id=0, language_id=0,
+            coverage_type_id=0, referral_type_id=0,
+            permanent_address_id=0, temporary_address_id=0,
+            phone_1_id=0, phone_2_id=0,
+            insurance_card_front_id=0, insurance_card_back_id=0):
         self.id = patient_id
+        self.prefix = prefix
         self.first_name = first_name
         self.last_name = last_name
         self.date_of_birth = date_of_birth
@@ -111,21 +103,21 @@ class Patient(Base):
         self.need_visa = need_visa
         self.dates_available = dates_available
         self.need_interpretor = need_interpretor
-        self.insurance_card_front_id = insurance_card_front_id
-        self.insurance_card_back_id = insurance_card_back_id
+        self.language_id = language_id
+        self.referral_type_id = referral_type_id
         self.referral_first_name = referral_first_name
         self.referral_last_name = referral_last_name
-        self.permanent_address_id = permanent_address_id
-        self.temp_address_id = temp_address_id
-        self.phone_number_1_id = phone_number_1_id
-        self.phone_number_2_id = phone_number_2_id
         self.preferred_contact_id = preferred_contact_id
-        self.language_id = language_id
         self.coverage_type_id = coverage_type_id
-        self.referral_type_id = referral_type_id
+        self.permanent_address_id = permanent_address_id
+        self.temporary_address_id = temporary_address_id
+        self.phone_1_id = phone_1_id
+        self.phone_2_id = phone_2_id
+        self.insurance_card_front_id = insurance_card_front_id
+        self.insurance_card_back_id = insurance_card_back_id
 
     def __repr__(self):
-        return f'Patient class = {self.id}, {self.first_name}, \
+        return f'Patient class = {self.id}, {self.prefix}, {self.first_name}, \
             {self.last_name}, {self.date_of_birth}, \
             {self.gender}, {self.age}, \
             {self.parent_name}, {self.email}, \
@@ -133,11 +125,11 @@ class Patient(Base):
             {self.dates_available}, {self.need_interpretor}, \
             {self.insurance_card_front}, {self.insurance_card_back}, \
             {self.referral_first_name}, {self.referral_last_name}, \
-            {self.permanent_address_id}, {self.temp_address_id}, \
-            {self.phone_number_1_id}, {self.phone_number_2_id}, \
             {self.preferred_contact_id}, {self.language_id}, \
             {self.coverage_type_id}, {self.referral_type_id}, \
-            {self.image_front_id}, {self.image_back_id}'
+            {self.permanent_address_id}, {self.temp_address_id}, \
+            {self.phone_1_id}, {self.phone_2_id}, \
+            {self.insurance_card_front_id}, {self.insurance_card_back_id}'
 
 
     def parse_patient_data(self, req):
@@ -161,10 +153,6 @@ class Patient(Base):
         self.age = req['q74_age']
         self.parent_first_name = req['q64_parentName']['first']
         self.parent_last_name = req['q64_parentName']['last']
-        self.permanent_address_id = _PERM_ADDRESS_ID
-        self.temp_address_id = _TEMP_ADDRESS_ID
-        self.phone_number_1_id = _PHONE_1_ID
-        self.phone_number_2_id = _PHONE_2_ID
         self.email = req['q35_email']
         self.preferred_contact_id = get_id_by_item(database, PreferredContact, \
             'preferred_contact', req['q37_preferredContact'])
@@ -177,11 +165,16 @@ class Patient(Base):
         self.need_interpretor = 1 if req['q78_areYou'] == "Yes" else 0
         self.language_id = get_id_by_item(database, Language, 'language', req['q70_If'])
         self.coverage_type_id = get_id_by_item(database, CoverageType, 'coverage_type', req['q50_typeA'])
-        self.insurance_card_front_id = _IMAGE_FRONT_ID
-        self.insurance_card_back_id = _IMAGE_BACK_ID
         self.referral_type_id = get_id_by_item(database, ReferralType, 'referral_type', req['q53_howDid'])
         self.referral_first_name = req['q67_For67']['first']
         self.referral_last_name = req['q67_For67']['last']
+        self.permanent_address_id = _PERM_ADDRESS_ID
+        self.temp_address_id = _TEMP_ADDRESS_ID
+        self.phone_number_1_id = _PHONE_1_ID
+        self.phone_number_2_id = _PHONE_2_ID
+        self.insurance_card_front_id = _IMAGE_FRONT_ID
+        self.insurance_card_back_id = _IMAGE_BACK_ID
+
         add_entity(database, Patient)
 
 class Address(Base):
@@ -197,7 +190,7 @@ class Address(Base):
     zip_code = Column('ZipCode', String(255), nullable=False)
     country_id = Column('CountryId', ForeignKey('Countries.Id'), nullable=False)
 
-    country = relationship('Countries')
+    patient = relationship('Patient', backref='address')
 
     def __init__(self, address_id=None, street_1=None, street_2=None,
         city=None, state=None, zip_code=None, country_id=0):
@@ -253,7 +246,7 @@ class Phone(Base):
     phone_number = Column('PhoneNumber', String(255), nullable=False)
     phone_type_id = Column('PhoneTypeId', ForeignKey('PhoneTypes.Id'), nullable=True)
 
-    phone_type = relationship('PhoneTypes')
+    patient = relationship('Patient', backref='phone')
 
     def __init__(self, phone_id=None, phone_number=None, phone_type_id=0):
         self.id = phone_id
@@ -294,6 +287,8 @@ class PhoneType(Base):
     id = Column('Id', Integer, primary_key=True)
     phone_type = Column('PhoneType', String(255), nullable=False)
 
+    phone = relationship('Phone', backref='phone_type')
+
 
     def __init__(self, phone_type_id=None, phone_type=None):
         self.id = phone_type_id
@@ -310,6 +305,8 @@ class PreferredContact(Base):
     __tablename__ = 'PreferredContacts'
     id = Column('Id', Integer, primary_key=True)
     preferred_contact = Column('ContactTypes', String(255), nullable=False)
+
+    patient = relationship('Patient', backref='preferred_contact')
 
 
     def __init__(self, preferred_contact_id=None, preferred_contact=None):
@@ -329,6 +326,8 @@ class Country(Base):
     id = Column('Id', String(255), primary_key=True)
     country = Column('Country', String(255), nullable=False)
 
+    address = relationship('Address', backref='country')
+
     def __init__(self, country_id=None, country=None):
         self.id = country_id
         self.country = country
@@ -344,6 +343,8 @@ class Language(Base):
     __tablename__ = 'Languages'
     id = Column('Id', Integer, primary_key=True)
     language = Column('Language', String(255), nullable=False)
+
+    patient = relationship('Patient', backref='language')
 
     def __init__(self, language_id=None, language=None):
         self.id = language_id
@@ -361,6 +362,8 @@ class CoverageType(Base):
     id = Column('Id', Integer, primary_key=True)
     coverage_type = Column('CoverageType', String(255), nullable=False)
 
+    patient = relationship('Patient', backref='coverage_type')
+
     def __init__(self, coverage_type_id=None, coverage_type=None):
         self.id = coverage_type_id
         self.coverage_type = coverage_type
@@ -375,6 +378,8 @@ class ReferralType(Base):
     __tablename__ ='ReferralTypes'
     id = Column('Id', Integer, primary_key=True)
     referral_type = Column('ReferralType', String(255), nullable=False)
+
+    patient = relationship('Patient', backref='referral_type')
 
     def __init__(self, referral_type_id=None, referral_type=None):
         self.id = referral_type_id
@@ -400,8 +405,6 @@ class Intake(Base):
     submission_id = Column('SubmissionId', String(255), nullable=True)
     form_title = Column('FormTitle', String(255), nullable=True)
 
-    patient = relationship('Patients', backref='Intakes', cascade='all, delete, delete-orphan')
-
     def __init__(self, intake_id=None, intake_date=None,
         intake_by_first_name=None, intake_by_last_name=None,
         liason_first_name=None, liason_last_name=None, patient_id=0,
@@ -424,7 +427,7 @@ class Intake(Base):
             {self.form_id}, {self.submission_id}, {self.form_title}'
 
 
-    def parse_Intake_data(self, form_data):
+    def parse_intake_data(self, form_data):
         """
         Parse the data from the form.
 
@@ -448,6 +451,7 @@ class Intake(Base):
             self.intake_by_last_name = req['q60_intakeBy']['last']
             self.liason_first_name = req['q62_liason']['first']
             self.liason_last_name = req['q62_liason']['last']
+            self.patient_id = _PATIENT_ID
             add_entity(database, Intake)
 
 class Image(Base):
@@ -459,6 +463,8 @@ class Image(Base):
     image_name = Column('ImageName', String(255), nullable=False)
     image_url = Column('ImageUrl', String(500), nullable=False)
     widget_type = Column('WidgetType', String(255), nullable=False, default='imagelinks')
+
+    patient = relationship('Patient', backref='image')
 
     def __init__(self, image_id=None, image_name=None, image_url=None, widget_type=None):
         self.id = image_id
@@ -494,40 +500,3 @@ class Image(Base):
             self.image_url = metadata['url']
             self.widget_type = "imagelinks"
         add_entity(database, Image)
-
-class ResponseObject():
-    """
-    This class is used to store the response from the API.
-    """
-    patient = None
-    address = None
-    phone = None
-    phone_type = None
-    image = None
-    preferred_contact = None
-    country = None
-    language = None
-    coverage_type = None
-    referral_type = None
-    intake = None
-
-    def __init__(self, patient:Patient, address:Address, phone:Phone,
-        phone_type:PhoneType, image:Image, preferred_contact:PreferredContact,
-        country:Country, language:Language, coverage_type:CoverageType,
-        referral_type:ReferralType, intake:Intake):
-        self.patient = patient
-        self.address = address
-        self.phone = phone
-        self.phone_type = phone_type
-        self.image = image
-        self.preferred_contact = preferred_contact
-        self.country = country
-        self.language = language
-        self.coverage_type = coverage_type
-        self.referral_type = referral_type
-        self.intake = intake
-
-    def __repr__(self):
-        return f'response_object class = {self.patient}, {self.address}, \
-            {self.phone}, {self.phone_type}, {self.image}, {self.preferred_contact}, \
-                {self.country}, {self.language}, {self.coverage_type}, {self.intake}'
